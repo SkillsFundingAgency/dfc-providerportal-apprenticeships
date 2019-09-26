f2121bfa0e2fbd07bfda240e29ddb37605524a44
Liam Wright <mail@liam-wright.co.uk>
2019-09-26 11:23:04 +0100

    Added bulk upload error list

---
+++ src/Dfc.ProviderPortal.Apprenticeships/Interfaces/Models/IBulkUploadError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dfc.ProviderPortal.Apprenticeships.Interfaces.Models
+{
+    public interface IBulkUploadError
+    {
+        int LineNumber { get; set; }
+        string Header { get; set; }
+        string Error { get; set; }
+    }
+}
---
+++ src/Dfc.ProviderPortal.Apprenticeships/Models/BulkUploadError.cs
@@ -0,0 +1,14 @@
+﻿using Dfc.ProviderPortal.Apprenticeships.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dfc.ProviderPortal.Apprenticeships.Models
+{
+    public class BulkUploadError : IBulkUploadError
+    {
+        public int LineNumber { get; set; }
+        public string Header { get; set; }
+        public string Error { get; set; }
+    }
+}
